90288eed9e1376a1f44228d163f62c71132f27ab
Marcus Neo <marcus121neo@gmail.com>
2023-08-19 21:22:55 +0800

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Marcus Neo on 19/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
